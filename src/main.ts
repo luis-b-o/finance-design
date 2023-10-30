af9ced92c52779fa28e528e8900345c8ce91242a
Luis Oliveira <luis.oliveira@startse.com>
2023-10-30 07:48:19 -0300

    add swagger module to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { AppModule } from '@/app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder().build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  app.enableShutdownHooks();
+
   await app.listen(3000);
 }
 bootstrap();
